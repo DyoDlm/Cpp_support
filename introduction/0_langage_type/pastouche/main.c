d774ce930d87d2377b34eb248afccd92ab8c6be3
dyodlm <baccus.delamancha@gmail.com>
2025-09-28 13:16:03 +0200

    mainongoing

--- introduction/0_langage_type/pastouche/main.c
+++ introduction/0_langage_type/pastouche/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <stdbool.h>
 
 static bool	test_ft_strlen()
@@ -26,8 +27,45 @@ static bool	test_ft_memcpy()
 	return true;
 }
 
+typedef struct enum
+{
+	STRLEN,
+	ISHOT,
+	UPPER,
+	LOWER,
+	MEMCPY,
+	OTHER
+};
+
+typedef int	(*t_test)(void);
+
 int	main(int ac, char **av)
 {
-	if (ac != 1
+	static const char	*tests[] = {
+		"ft_strlen", "isHot", "ft_strToUpper",
+		"ft_strToLower", "ft_memcpy", NULL
+	};
+	static t_test testers[] = {
+		test_ft_strlen, test_isHot, test_ft_strToUpper,
+		test_ft_strToLower, test_ft_memcpy, NULL
+	};
+	int	test = 0;
+	int	status;
+
+	if (ac < 2)
+		return 1;
+	for (int i = 0; i < OTHER; i++)
+	{
+		if (!strcmp(tests[i], av[1]))
+			break ;
+		test++;
+	}
+	
+	status = testers[test]();
+	
+	if (status)
+		printf("OK\n");
+	else
+		printf("KO\n");
 	return (0);
 }
