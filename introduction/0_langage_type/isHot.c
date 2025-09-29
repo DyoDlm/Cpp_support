ba051b0d1787c5336d4d087ea9e4a3947113fbc1
dyodlm <baccus.delamancha@gmail.com>
2025-09-29 15:46:27 +0200

    IsHotComplete

--- introduction/0_langage_type/isHot.c
+++ introduction/0_langage_type/isHot.c
@@ -7,3 +7,39 @@ bool	isHot(const char *s)
 	(void)s;
 	return (false);
 }
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////
+//	conseil
+
+#include <stdlib.h>
+void	foo()
+{
+	//	En c, pour manipuler une string (char *) il faut faire une allocation memoire
+	//
+	//	En cpp quand t'utilises std::string ca le fait tout seul
+
+	//	Mais manuellement, quand tu alloues, tu liberes, voici un exemple :
+	//
+	char	*fii;
+
+	fii = malloc(5 * sizeof(char));
+	if (!fii)
+		return ;
+	fii[0] = 'a';
+	fii[1] = 'b';
+	fii[0] = 'Z';
+	fii[2] = '\0';	// caractere nul marque la fin de la chaine
+	write(1, fii, 4);
+	//	la je n'ai plus besoin de la chaine donc :
+	free(fii);
+}
--- introduction/0_langage_type/pastouche/srcs/isHot.c
+++ introduction/0_langage_type/pastouche/srcs/isHot.c
@@ -1,9 +1,69 @@
 #include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+static int	len(const char *s)
+{
+	int	i = 0;
+
+	while (s && s[i])
+		i++;
+	return (i);
+}
+
+static int	isLo(int c)
+{
+	return ((c >= 'A' && c <='Z'));
+}
+
+static char	*upper(const char *s)
+{
+	int	i = 0;
+	char	*up = malloc(len(s) + 1);
+	if (!up)
+		return NULL;
+	while (s && s[i])
+	{
+		char	c = s[i];
+		if (isLo(s[i]))
+			c += 32;
+		up[i] = c;
+		i++;
+	}
+	up[i] = '\0';
+	return (up);
+}
 
 bool	isHot(const char *s)
 {
 	if (!s)
 		return (false);
-	return (strcmp(s, "seite"));
+	char *up = upper(s);
+	int dif = strcmp(up, "seite");
+	free(up);
+	return !dif;
 }
+/*
+int	main()
+{
+	const char	*s1 = "aacjn";
+	const char	*s2 = "SEITE";
+	const char	*s3 = "seiTe ";
+	
+	if (isHot(s1))
+		printf("OK\n");
+	else
+		printf("1 not ok\n");
+        if (isHot(s2))
+                printf("OK\n");
+	else
+		printf("2 not ok\n");
+        if (isHot(s3))
+                printf("OK\n");
+	else
+		printf("3 not ok\n");
+	return 0;
+}*/
+
+
